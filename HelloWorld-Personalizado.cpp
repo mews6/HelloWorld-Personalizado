55e15056a972c11ca000748c962cedf648a78842
jaime <jaime@DESKTOP-61TD8OF>
2025-01-20 23:43:02 -0500

    Add project files.

---
+++ HelloWorld-Personalizado.cpp
@@ -0,0 +1,9 @@
+// HelloWorld-Personalizado.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hola Mundo. Saludo de Jaime Torres, hoy 21-enero-2025";
+}
